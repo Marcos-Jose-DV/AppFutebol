d339ab7a13491fdff94b3563d13f5ce98aeac27e
Marcos José <marcosjoseDV@hotmail.com>
2023-09-13 00:53:03 -0300

    ok

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,87 +1,91 @@
 ﻿using AppFutebol.Controlls;
-using AppFutebol.Models;
-using AppFutebol.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.Maui.Graphics.Text;
-using System.Collections.ObjectModel;
-using System.Text.Json;
 using System.Windows.Input;
 
 namespace AppFutebol.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
-    private readonly ConfigHttpService _config;
-    HttpClient _client;
-    JsonSerializerOptions _JsonSerializer;
+    private readonly IRestService _service;
 
     [ObservableProperty]
-    public MatchInfo[] _match;
+    public Match[] _match;
 
     [ObservableProperty]
     private bool _isBusy = true;
 
     [ObservableProperty]
-    private string _day,_mes, _date, _dateNow;
+    private string _day, _mes, _date, _dateNow;
 
     public ICommand BackDayCommand
-        => new Command(async () => await BackDay());
+        => new Command(BackDay);
 
     public ICommand NextDayCommand
-        => new Command(async () => await NextDay());
+        => new Command(NextDay);
 
+    public ICommand SearchCommand
+        => new Command(Search);
 
-
-    public MainViewModel()
+    
+    public MainViewModel(IRestService service)
     {
-        _config = new ConfigHttpService();
-        _client = new HttpClient();
-        _JsonSerializer = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        GetMatchInfoAsync(DateTime.Now.ToString("yyyy-MM-dd"));
+        _service = service;
+        NewDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+    }
+    private void Search()
+    {
+        throw new NotImplementedException();
     }
 
-    private async Task BackDay()
+    private void BackDay()
     {
         Match = null;
         var date = DateTime.Parse(DateNow).AddDays(-1);
-        await GetMatchInfoAsync(date.ToString("yyyy-MM-dd"));
+        NewDateTime(date.ToString("yyyy-MM-dd"));
     }
-    private async Task NextDay()
+    private void NextDay()
     {
         Match = null;
         var date = DateTime.Parse(DateNow).AddDays(1);
-        await GetMatchInfoAsync(date.ToString("yyyy-MM-dd"));
+        NewDateTime(date.ToString("yyyy-MM-dd"));
     }
 
-    private async Task GetMatchInfoAsync(string date)
+    private void NewDateTime(string date)
+    {
+        Date = Convert.ToString(DateTime.Parse(date).Day);
+        DateNow = date;
+        Mes = Convert.ToString(DateTime.Parse(date).ToString("MM-yyyy"));
+        Day = DayControll.GetDayPtBR(DateTime.Parse(date));
+        IsBusy = true;
+        GetStandingsData();
+    }
+
+    private async Task<Task> GetStandingsData()
     {
         try
         {
-            IsBusy = true;
-            Date = Convert.ToString(DateTime.Parse(date).Day);
-            DateNow = date;
-            Mes = Convert.ToString(DateTime.Parse(date).ToString("MM-yyyy"));
-            Day = DayControll.GetDayPtBR(DateTime.Parse(date));
-
-            var url = $"{_config.Url}_events&from={DateNow}&to={DateNow}&league_id=99&APIkey={_config.Key}&timezone=America/Sao_Paulo";
-            var response = await _client.GetAsync(url);
-
-            if (response.IsSuccessStatusCode)
-            {
-                using var responseStream = await response.Content.ReadAsStreamAsync();
-                var data = await JsonSerializer.DeserializeAsync<MatchInfo[]>(responseStream, _JsonSerializer);
-                Match = data;
-            }
-        }
-        catch (Exception ex)
-        {
-            ex.ToString();
+            Match[] match = await _service.GetMatchData(GenerateRequestURL(Constants.Opefootball));
+            Match = match;
         }
         finally
         {
             IsBusy = false;
-
         }
+
+        return Task.CompletedTask;
+    }
+
+    private string GenerateRequestURL(string endPoint)
+    {
+        string requestUri = endPoint;
+        requestUri += $"_events";
+        requestUri += $"&from={DateNow}";
+        requestUri += $"&to={DateNow}";
+        requestUri += $"&country_id=27";
+        requestUri += $"&APIkey={Constants.OpeFootballKey}";
+        requestUri += $"&timezone=America/Sao_Paulo";
+
+        return requestUri;
     }
 }
 
